69549faf47fd52c3b2d6aaa49d9095dc98f97ddc
Brandon Carter <bcarteratx@gmail.com>
2020-03-12 11:28:50 -0500

    routes

--- controllers/event.js
+++ controllers/event.js
@@ -1,8 +1,24 @@
+const Event = require('../models/event');
 
 module.exports = {
-    index
+    index,
+    new: newEvent,
+    show,
+    create,
+    delete: deleteOne,
+    edit,
+    update
 }
 
 function index(req, res) {
     res.render('index');
+}
+
+function newEvent(req, res) {
+    res.render('events/new');
+}
+
+function show(req, res, idx) {
+    
+    res.render('events/show');
 }
